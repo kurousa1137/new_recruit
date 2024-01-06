100677ae0eb0f70ab38c72464e3c34007bb921f8
sugimoto <d.sugimoto0309@gmail.com>
2024-01-06 17:48:05 +0900

    [update]フッターにリンク埋め込み

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -6,9 +6,9 @@
       <img src="{{ asset('img/common/logo_f.svg') }}" alt="">
       <div>
         <ul>
-          <li><a href="">公式サイト</a></li>
-          <li><a href="">プライバシーポリシー</a></li>
-          <li><a href="">お問い合わせ</a></li>
+          <li><a href="https://www.csauto.jp" target="_blank">公式サイト</a></li>
+          <li><a href="https://www.csauto.jp/privacy-policy" target="_blank">プライバシーポリシー</a></li>
+          <li><a href="https://www.csauto.jp/inquire" target="_blank">お問い合わせ</a></li>
         </ul>
         <small>CS AutoDealer Inc. All  rights reserved.</small>
       </div>
