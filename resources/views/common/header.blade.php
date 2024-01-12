f16b22473417585d84c3456b3d53e88ee8dd19d8
sugimoto <d.sugimoto0309@gmail.com>
2024-01-12 15:29:17 +0900

    [update]ディスクリプション追加

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="">
+  <meta name="description" content="創業43年！関東最大級の中古車専門店！ 新たなサービス展開、さらなる会社の基盤強化を目指すために一緒に未来を創る仲間を募集します。">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap" rel="stylesheet">
